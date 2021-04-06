831d0d1937625500f296a22b89cb5382e9a5f847
isriom <62965230+isriom@users.noreply.github.com>
2021-04-06 14:40:34 -0600

    Change junit to 4.13.2

--- src/test/java/Calculator/TestCalculator.java
+++ src/test/java/Calculator/TestCalculator.java
@@ -1,33 +1,31 @@
 package Calculator;
 
-import Calculator.Calculator;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import junit.framework.TestCase;
 
-public class TestCalculator{
+
+public class TestCalculator extends TestCase{
 
     public Calculator calculadora= new Calculator();
 
-    @Test
-    void test_Suma(){
-        assertEquals(2, Calculator.add(1,1));
-        assertEquals(2, Calculator.add(3,-1));
+    public void test_Suma(){
+        assertEquals(2.0, Calculator.add(1,1));
+        assertEquals(2.0, Calculator.add(3,-1));
 
     }
-    @Test
-    void test_Divide(){
 
-        assertEquals(2, Calculator.divide(2,1));
-        assertEquals(1, Calculator.divide(5,5));
+    public void test_Divide(){
+
+        assertEquals(2.0, Calculator.divide(2,1));
+        assertEquals(1.0, Calculator.divide(5,5));
     }
-    @Test
-    void test_Pwr(){
-        assertEquals(2, Calculator.pwr(2,1));
-        assertEquals(8, Calculator.pwr(2,3));
+
+    public void test_Pwr(){
+        assertEquals(2.0, Calculator.pwr(2,1));
+        assertEquals(8.0, Calculator.pwr(2,3));
     }
-    @Test
-    void test_Multiply(){
-        assertEquals(2, Calculator.multiply(2,1));
-        assertEquals(100, Calculator.multiply(50,2));
+
+    public void test_Multiply(){
+        assertEquals(2.0, Calculator.multiply(2,1));
+        assertEquals(100.0, Calculator.multiply(50,2));
     }
 }
